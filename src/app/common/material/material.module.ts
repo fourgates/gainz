94e416459fb238a49e48050eaa95dc717449c1b0
PhillipNinan <ninan.phillip@gmail.com>
2020-03-21 20:30:55 -0400

    added the button material module

--- src/app/common/material/material.module.ts
+++ src/app/common/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [],
+  exports: [MatButtonModule],
   imports: [
     CommonModule
   ]
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './container/home.component';
-
-
+import { MaterialModule } from './../../common/material/material.module'
 
 @NgModule({
   declarations: [HomeComponent],
   exports: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class HomeModule { }
