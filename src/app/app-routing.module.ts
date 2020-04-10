58aa376abb8f2cf0d83203bd76a3942984a6d165
PhillipNinan <ninan.phillip@gmail.com>
2020-04-10 00:39:46 -0400

    setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './modules/home/container/home.component';
+import { LoginComponent } from './modules/admin/login/login.component';
+import { AuthGuard } from './modules/auth/guards/auth-guard.guard'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: HomeComponent, canActivate: [AuthGuard]},
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
